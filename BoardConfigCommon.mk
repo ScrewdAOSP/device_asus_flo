81f6164704a152a854da5e0d228835941a4530ad
MrApocalypse <ensabahnur16@gmail.com>
2015-01-01 23:52:16 -0600

    Add SCREWD flag for optimizing
    Gotta have this tp make it work:
    https://github.com/MrApocalypse/android_build/commit/f3d5b2815661bd959cbdea54c39a293126038d77

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,6 +37,9 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 TARGET_KERNEL_SOURCE := kernel/google/msm
 TARGET_KERNEL_CONFIG := screwd_flo_defconfig
 
+#Screw it up!
+SCREWD:= true
+
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
 BOARD_USES_FLUENCE_INCALL := true
